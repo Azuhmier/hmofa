e9fd733bd30a91e79fb3e2407df572e7c3bc6a59
Azuhmier <Azuhmier@Azuhmiers-MBP.hitronhub.home>
2020-12-20 17:21:15 -0600

    tags.pl needs some fixin

--- code/tags.pl
+++ code/tags.pl
@@ -19,3 +19,39 @@ use List::MoreUtils 'uniq';
 use lib ($ENV{HOME}.'/hmofa/hmofa/code/lib');
 no warnings 'utf8';
 
+my $d = ',';
+my $nb = qr/[^\]\[]/;
+my $dspt = {
+  section => {
+    order => '1a',
+    re => qr/^\s+%+\s+(.*)\s+%+/,
+  },
+  author => {
+    order => '2a',
+    re => qr/^\s*By\s+(.*)/,
+  },
+  series => {
+    order => '3a',
+    re => qr/^=+\/\s*(.*)\s*\/=+/,
+  },
+  title => {
+    order => '4a',
+    re => qr/^(\s*)>\s*(.*)/,
+  },
+  tags => {
+    order => '4b',
+    re => qr{(?x)
+      ^\s*\[($nb*)\]\s*\[($nb*)\]($nb*)\n$
+      |^\s*\[($nb*)\]($nb*)\n$
+      |^\s*([~OX\$\*]+)\s*\n$
+    },
+  },
+  url => {
+    order => '4c',
+    re => qr/(https?:\/\/[^\s]+)\s+(.*)/,
+  },
+  description => {
+    order => '4d',
+    re => qr/^#(.*)/,
+  },
+};
