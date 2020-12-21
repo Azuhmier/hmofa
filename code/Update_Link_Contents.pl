9f72d9a44c096fcb9e886f6035b54a799f31d453
Azuhmier <Azuhmier@Azuhmiers-MBP.hitronhub.home>
2020-12-21 11:37:30 -0600

    updating links

---
+++ code/Update_Link_Contents.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl;
+use warnings;
+use strict;
+use LWP::Simple;
+use LWP::UserAgent;
+use HTML::SimpleLinkExtor;
+use List::MoreUtils 'uniq';
+use File::Find;
+use File::Path;
+use File::Basename;
+use File::Spec;
+use File::Glob;
+use File::Copy;
+use Cwd;
+use utf8;
+no warnings 'utf8';
+
+my $head = glob('~/hmofa/hmofa');
+my $cwd = getcwd;
+my $archive = $head."/archive_7";
+my $RecentScrapes = $head."/Recent_Scrapes";
+my @pastes=GetPasteKeys($archive);
+my @scrapes=GetPasteKeys($RecentScrapes."/archive");
+#my ($filename, $dirs, $suffix) = finfo_display($cwd."/ll.iuo");
+#for my $file (@scrapes) {
+#  ReturnValidPastes($RecentScrapes."/archive/".$file);
+#}
+ReturnValidPastes($RecentScrapes."/archive",\@scrapes);
+  print "\n";
+ReturnValidPastes($archive,\@pastes);
+
+sub finfo_display {
+  my $cwd = shift @_;
+  my ($filename, $dirs, $suffix) = fileparse($cwd);
+  print "fname: $filename\n";
+  print "dir: $dirs\n";
+  print "suffix: $suffix\n";
+  return [$filename, $dirs, $suffix];
+}
+
+sub GetPasteKeys {
+  my $dirname = shift @_;
+  my @PasteKeys;
+  opendir (my $dh, $dirname) || die "Error in opening dir $dirname\n";
+  {
+    #while( ($filename = readdir($dh))) {
+    #  print("$filename\n");
+    #}
+    @PasteKeys = grep { /^[^.]+/ } readdir($dh);
+    #print join "\n", @PasteKeys;
+  }
+  closedir $dh;
+  return @PasteKeys;
+}
+
+sub ReturnValidPastes {
+  my $dir = shift;
+  my $PasteKeys = shift;
+  my @PasteKeys =@$PasteKeys;
+  my @FilePaths = map {$dir."/".$_ } @PasteKeys;
+  for my $file (@FilePaths) {
+    my $pastekey = shift @PasteKeys;
+    open my $fh, '<', $file or die "$0: $file: No such file\n";
+      while (my $line = <$fh>) {
+          if ($line =~ /<h1>.*\(#40[34]\)<\/h1>/) { 
+            print "$pastekey\n";
+          }
+      }
+   }
+}
+
---
+++ code/scrapping/Paste_request.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl;
+use warnings;
+use strict;
+use LWP::Simple;
+use LWP::UserAgent;
+use HTML::SimpleLinkExtor;
+use List::MoreUtils 'uniq';
+use utf8;
+no warnings 'utf8';
+
+# Set up LWP object
+my $ua = LWP::UserAgent->new(timeout => 10);
+$ua->agent('Mozilla/5.0');
+
+# Start timer
+my $start = time;
+
+# Get urls from 'url_only.txt'
+my $count = 0;
+my @urls;
+my $fh;
+my $fname = glob('~/hmofa/hmofa/code/urls.txt');
+
+while ($count <= 8) {
+  my $response = $ua->get('https://desuarchive.org/trash/search/text/hmofa/type/op/page/' . $count .'/');
+
+  unless ( $response->is_success ) {
+    die $response->status_line;
+  }
+
+  my $html = $response->decoded_content;
+  my $extractor = HTML::SimpleLinkExtor->new; $extractor->parse($html);
+  my @links = $extractor->links;
+  for my $link (uniq grep {m/https:\/\/paste/} @links ) {
+    if ($link !~ m/https:\/\/pastebin.com\/raw[^\s]+/) {
+      $link =~ m/https:\/\/pastebin.com\/[^\s]+/;
+      my $match = $&;
+      $match =~ s/(https:\/\/pastebin.com\/)([^\s]+)/$1raw\/$2/;
+      $link = $match;
+    }
+    push @urls, $link;
+  }
+  $count++;
+}
+
+# Get urls from 'url_only.txt'
+my $fname_urls = glob('~/hmofa/hmofa/code/goto_files/url_only.txt');
+open $fh, '<' , $fname_urls ;
+  while (my $line = <$fh>) {
+    $line =~ m/https:\/\/pastebin.com\/[^\s]+/;
+    my $match = $&;
+    $match =~ s/(https:\/\/pastebin.com\/)([^\s]+)/$1raw\/$2/;
+    push @urls, $match;
+  }
+close $fh;
+@urls = uniq @urls;
+
+# Check if $dirname is available
+my $dirname = 'archive';
+my $DestFolder = glob('~/hmofa/hmofa/Recent_Scrapes/' . $dirname);
+my $FileNumber = 2;
+
+if (-d $DestFolder) {
+  while (-d $DestFolder . '_' . $FileNumber ) {
+    $FileNumber++;
+  }
+  $DestFolder = $DestFolder . '_' . $FileNumber;
+}
+
+print $DestFolder;
+mkdir $DestFolder;
+
+#Store files
+my @fh; # will store the filehandles.
+my @html;
+my $i=0;
+for my $link (@urls) {
+    my $response = $ua->get($link);
+    $link =~ s/https:\/\/pastebin.com\/raw\/([^\s]+)/$1/;
+    my $name = $1;
+    push @html, $response->decoded_content;
+    my $start = time;
+
+    open $fh[$i], '>', $DestFolder . "/$name" or die $!;
+      if ($html[$i]) {
+      print {$fh[$i]} $html[$i];
+      }
+      else {
+        my $DeadString = $_ . "\n" . "DEAD_LINK";
+      print {$fh[$i]} $DeadString;
+      }
+    close $fh[$i];
+
+    $i++;
+    while ( (time - $start) < 2){}
+}
+
+my $duration = time - $start; print "\nExecution Time: $duration s\n";
+
+
+
+
