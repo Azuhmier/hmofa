14c186fea2d708f936cf2f334f908a882401a577
Azuhmier <Azuhmier@Azuhmiers-MBP.hitronhub.home>
2020-12-23 18:39:25 -0600

    fixing some links

---
+++ code/scrapping/sofurry2.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl;
+use warnings;
+use strict;
+use LWP::Simple;
+use LWP::UserAgent;
+use utf8;
+
+my $ua = LWP::UserAgent->new(timeout => 10);
+$ua->agent('Mozilla/5.0'); 
+my $start = time;
+
+my $response = $ua->get('http://api2.sofurry.com/std/getUserProfile');
+print $response->decoded_content;
