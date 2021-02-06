22f8fb2b03bddfe4f658cf1ab0c42d7315a946f6
Azuhmier <Azuhmier@Azuhmiers-MBP.hitronhub.home>
2021-02-06 16:11:36 -0600

    >filename

--- code/scrapping/sofurry2.pl
+++ code/scrapping/sofurry2.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl;
 use warnings;
 use strict;
-use LWP::Simple;
+#use LWP::Simple;
 use LWP::UserAgent;
 use utf8;
 
---
+++ getgoogledocs.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+#============================================================
+#
+#        FILE: getgoogledocs.pl
+#
+#       USAGE: ./getgoogledocs.pl
+#
+#  DESCRIPTION: ---
+#
+#       AUTHOR: Azuhmier (aka taganon), azuhmier@gmail.com
+# ORGANIZATION: ---
+#      VERSION: 1.0?
+#      Created: https://pdf.co/how-to-get-direct-download-links
+#============================================================
+use strict;
+use warnings;
+use LWP::UserAgent;
+
+# define the googledoc ID's
+my $masterbin_ID   = '1uTaenno7gn5ZCX5X4NPCcWRqTa_U4d7rtOXZOMuIT0k';
+my $download_url   = "https://docs.google.com/document/d/".$masterbin_ID."/export?format=txt";
+
+# get googledoc as txt
+my $ua = LWP::UserAgent->new(timeout => 10);
+$ua->agent('Mozilla/5.0');
+my $response = $ua->get($download_url);
+
+# clean up txt and export to file
+my $txt = $response->decoded_content;
+$txt =~ s///g;
+$txt =~ s/\n\n\n+/\n\n/g;
+open(my $fh, '+<', 'masterbin.txt') or die;
+  print $fh $txt;
+  seek $fh,0,0 or die;
+  my @COPY;
+  while (my $line = <$fh>) {
+    $line =~ s/^\[\w{1,2}\].[^\]]*$//;
+    $line =~ s/\[\w{1,2}\]$//;
+    $line =~ s/\s+(\n)$/$1/; # '\s' includes newlines, contrary to what it does in vim
+    push @COPY, $line;
+  }
+  seek $fh,0,0 or die;
+  for my $line (@COPY) {
+    print $fh $line;
+  }
+  truncate $fh, tell($fh) or die;
+close $fh;
+
+##----- REFFS -----
+## https://pdf.co/how-to-get-direct-download-links
+## (not used) https://perlmaven.com/slurp
