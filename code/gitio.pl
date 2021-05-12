f272776e601828cdc86cb98dfa6e8cb46d0e2456
Azuhmier <Azuhmier@Azuhmiers-MacBook-Pro.local>
2021-05-12 13:08:22 -0500

    updating

--- code/gitio.pl
+++ code/gitio.pl
@@ -8,47 +8,77 @@
 #============================================================
 use warnings;
 use strict;
+use Data::Dumper;
 
-# parse cmdline arguments and output hash
-my %args = ArgParser(\@ARGV);
+#--- MAIN ----------------------------------------{{{1
+#parse cmdline arguments and output 'args' hash
+my $args = ParseArgs(\@ARGV);
 
-my $cmd = q{curl -s -i https://git.io -F "url=}.$args{'URL'}.q{" \\
-  | grep Location                                                \\
-  | sed -n 's/Location: \(.*\)/\1/p'                             \\
-  | tr -d '\n'                                                   \\
-  | tee /dev/tty                                                 \\
-  | tr -d '\r\n'                                                 \\
-  | pbcopy};
-my $output = `$cmd`;
-print `$output`, "\n";
+#construct system command
+my $CMD = constructCMD($args);
 
-sub ArgParser{
+#execute sys cmd
+my $output = qx( $CMD );
+if ( !$args->{'silent'} ) { print $output, "\n" }
+
+
+#--- SUBROUTINES ----------------------------------{{{1
+sub ParseArgs {
   my @args = @{shift @_};
-  my %args;
 
-  # - 3 arg MAX
-    my $argNUM = scalar @args;
+  #Parameters
+  my %valid_urls = (
+    io => 'https://git.io/',
+    raw => 'https://raw.githubusercontent.com/' );
+  my %valid_opts = (
+    clipboard => '-cb',
+    slient => '-s' );
 
-  # - url must come last
-  # - 'https://git.io/' or 'https://raw.githubusercontent.com/' must match
-    my $ioURL  = 'https://git.io/';
-    my $rawURL = 'https://raw.githubusercontent.com/';
-    if ($args[-1] =~ qr{\Q$ioURL\E|\Q$rawURL\E} ) { 
-        $args{'URL'} = $args[-1];
-
+  #Option Check
+  my %opts;
+  for my $arg ( @args[0..$#args-1] ) {
+    my @match = grep { $arg =~ /\Q$valid_opts{$_}\E/ } keys %valid_opts;
+    if ( exists ( $opts{ $match[0] } ) ) {
+      die( "'$arg' can only be stated once!! called at ${0} line ", __LINE__, "\n" )
     }
+    $opts{ $match[0] } = ( scalar @match == 1 ) ? $valid_opts{ $match[0] } :
+      die( "'$arg' is not a valid option ${0} line ", __LINE__, "\n" );
+  }
 
+  #URL must be valid and last
+  my @match = grep { $args[-1] =~ /^\Q$valid_urls{$_}\E/  } keys %valid_urls;
+  my $url   = scalar @match == 1 ? $args[-1] :
+      die( "URL is invalid or not the last argument! called at ${0} line ", __LINE__, "\n" );
+  my $type  = $match[0];
 
-  # - [-s], slience output, or [-c], copy output to clipboard, are the only valid
-  #   options and must not repeat
-    my @opts = @args[0..$#args-1];
-    for my $opt (@opts) {
-      print $opt,"\n";
-    }
-
-  # - failure to meet the above guidlines will result in termination of the
-  #   program
-
-  return %args;
+  #Return Hash
+  return {
+    URL  => $url,
+    type => $type,
+    %opts,
+  }
 }
 
+sub constructCMD {
+  my %args = %{shift @_};
+  my $CMD;
+  #xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  if ($args->{'type'} =~ 'raw') {
+    $CMD = q{curl -s -i https://git.io -F "url=}.$args->{'URL'}.q{" };
+  }
+  else {
+    $CMD = q{curl -s -i }.$args->{'URL'};
+  }
+  #xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  my $cmd2 = q{ | grep Location        \\
+    | sed -n 's/Location: \(.*\)/\1/p' \\
+    | tr -d '\n'                       \\
+    | tee /dev/tty                     \\
+    | tr -d '\r\n' };
+  #xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  my $cmd3 = !$args->{'clipboard'} ? '' : '| pbcopy';
+  #xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  $CMD = $CMD.$cmd2.$cmd3;
+  #xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  return $CMD;
+}
--- code/tags3.pl
+++ code/tags3.pl
@@ -2,14 +2,9 @@
 #============================================================
 #
 #        FILE: tags.pl
-#
 #       USAGE: perl ./tags.pl
-#
 #  DESCRIPTION: ---
-#
 #       AUTHOR: Azuhmier (aka taganon), azuhmier@gmail.com
-# ORGANIZATION: ---
-#      VERSION: 1.0
 #      Created: Sun 12/20/20 16:47:38
 #===========================================================
 
@@ -19,169 +14,186 @@ use utf8;
 use XML::Simple;
 use JSON;
 use Data::Dumper;
+$Data::Dumper::Indent = 1;
 use Storable qw(dclone);
 
 #----- FILEPATHS -----{{{1
-my $fname_MasterBin = '../masterbin.txt';
-my $fname_MasterBinXML = '../masterbin.xml';
-my $fname_MasterBinjson = '../masterbin.json';
+my $fname_IN = '../tagCatalog.txt';
 
 
 #----- REGEX CONFIG -----{{{1
-my $d = ',';
-my $nb = qr/[^\]\[]/;
-my $logger = {};
 my $dspt = {
-  #section => {
-  #  order => '1a',
-  #  re => qr/^\s+%+\s+(.*)\s+%+/,
-  #  match => [],
-  #},
-  author => {
-    order => '2a',
-    re => qr/^\s*By\s+(.*)/,
+  section => {
+    order => '1a',
+    re => qr/^\s*%+\s*(.*?)\s*%+/,
     match => [],
   },
-  #AuthorBorder => {
-  #  order => '2a',
-  #  re => qr/^---+$/,
-  #  match => [],
-  #},
-  #series => {
-  #  order => '3a',
-  #  re => qr/^=+\/\s*(.*)\s*\/=+/,
-  #  match => [],
-  #},
+  author => {
+    order => '2a',
+    re => qr/^\s*[Bb]y\s+(.*)/,
+    match => [],
+  },
+  series => {
+    order => '3a',
+    re => qr/^=+\/\s*(.*)\s*\/=+/,
+    match => [],
+  },
   title => {
     order => '4a',
     re => qr/^\s*>\s*(.*)/,
     match => [],
   },
-  #tags => {
-  #  order => '5a',
-  #  re => qr{(?x)
-  #    ^\s*\[($nb*)\]\s*\[($nb*)\]($nb*)\n$
-  #    |^\s*\[($nb*)\]($nb*)\n$
-  #    |^\s*([~OX\$\*]+)\s*\n$
-  #  },
-  #  match => [],
-  #},
+  tags => {
+    order => '5a',
+    re => qr/^\s*(\[.*)/,
+    match => [],
+  },
   url => {
     order => '5b',
     re => qr/(https?:\/\/[^\s]+)\s+(.*)/,
     match => [],
   },
-  #description => {
-  #  order => '5c',
-  #  re => qr/^#(.*)/,
-  #  match => [],
-  #},
+  description => {
+    order => '5c',
+    re => qr/^#(.*)/,
+    match => [],
+  },
 };
 
-#----- FILEHANDLING -----{{{1
-open(my $fh_MasterBin, '<' , $fname_MasterBin) or die $!; #Open Masterbin for reading
-  while (my $line = <$fh_MasterBin>) {
-    for my $key (keys %$dspt) {
-      my $key_reff = $dspt->{$key};
-      if ($key_reff->{re} && $line =~ /$key_reff->{re}/) {
-        my $hash = {
-          LN => $.,
-          contents => $1,
-        };
-        push  @{$key_reff->{match}}, $hash;
+my @a = grep {$dspt->{$_}->{order} } keys %$dspt;
+
+print Dumper(\@a);
+#----- Main -----{{{1
+my $capture_hash = file2hash( $fname_IN );
+#my $formated_hash = formatHash( $capture_hash, $dspt );
+
+#----- Subroutines -----{{{1
+sub file2hash {
+  my $fname = shift @_;
+  my $output;
+  open( my $fh, '<', $fname )  #Open Masterbin for reading
+    or die $!;
+
+    while ( my $line = <$fh> ) {
+
+      for my $obj_key ( keys %$dspt ) {
+        my $obj = $dspt->{$obj_key};
+
+        if ( $line =~ /$obj->{re}/ ) {
+          my $match = {
+            LN       => $.,
+            $obj_key => $1, };
+          push  $output->{$obj_key}->@*, $match;
+
+        }
+      }
+    }
+
+  close($fh);
+
+  return $output;
+}
+
+sub formated_hash {
+  my $capture_hash = shift @_;
+  my $dspt = shift @_;
+  my $reff = generate_reffHash($dspt);
+  my $output = {};
+
+  for my $obj_key ( keys %$dspt ) {
+     my $obj = $dspt->{$obj_key};
+     push  @$reff, [ $obj->{order}, $obj_key ];
+  }
+
+  @$reff = sort { @$a[0] cmp @$b[0] } @$reff;
+
+  for my $author ( reverse $dspt->{author}->{match}->@* ) {
+    my $lineA = ${$author}{LN};
+    my $obj = $output->{ ${$author}{contents} };
+    $output->{ ${$author}{contents} } = { LN => ${$author}{LN} };
+
+    for my $b ( reverse $dspt->{title}->{match}->@*) {
+      my $lineB = ${$b}{LN};
+
+      if ( $lineA < $lineB ) {
+        my $nameB = ${$b}{contents};
+        $output->{ ${$author}{contents} }->{titles}->{$nameB}->{urls}= [];
+
+        for my $c ( reverse $dspt->{url}->{match}->@*) {
+          my $lineC = ${$c}{LN};
+
+          if ( $lineB < $lineC ) {
+            my $nameC = ${$c}{contents};
+            push $output->{ ${$author}{contents} }->{titles}->{$nameB}->{urls}->@*, $nameC;
+            pop $dspt->{url}->{match}->@*;
+          }
+          else {
+            last;
+          }
+        }
+        pop $dspt->{title}->{match}->@*;
+      }
+      else {
+        last;
       }
     }
   }
-close($fh_MasterBin);
-#----- HASH MANIPULATION -----{{{1
-my $reff = [];
-for my $key (keys %$dspt) {
-   my $key_reff = $dspt->{$key};
-   push  @$reff, [$key_reff->{order},$key];
-   delete $key_reff->{re};
-   delete $key_reff->{order};
 }
-@$reff = sort {@$a[0] cmp @$b[0]} @$reff;
-my $output = {};
-for my $a ( reverse $dspt->{author}->{match}->@*) {
-  my $lineA = ${$a}{LN};
-  my $key_reff = $output->{${$a}{contents}};
-  $output->{${$a}{contents}} = {LN => ${$a}{LN}};
-  #$output->{${$a}{contents}}->{titles} = {};
-  for my $b ( reverse $dspt->{title}->{match}->@*) {
-    my $lineB = ${$b}{LN};
-    if ($lineA < $lineB) {
-      my $nameB = ${$b}{contents};
-      $output->{${$a}{contents}}->{titles}->{$nameB}->{urls}= [];
-      for my $c ( reverse $dspt->{url}->{match}->@*) {
-        my $lineC = ${$c}{LN};
-        if ($lineB < $lineC) {
-          my $nameC = ${$c}{contents};
-          push $output->{${$a}{contents}}->{titles}->{$nameB}->{urls}->@*, $nameC;
-          pop $dspt->{url}->{match}->@*;
-        }
-        else {
-          last;
-        }
-      }
-      pop $dspt->{title}->{match}->@*;
-    }
-    else {
-      last;
-    }
-  }
+
+sub generate_reffHash {
 }
-#print Dumper($output);
-#----- BEAUTIFY -----{{{1
-  my $DP_output = dclone($output); # Deep Copy of $output
-  my $output2 = [];                # New Better Hash
-  for my $author (keys %$DP_output) {
-    my @titles;
-    for my $title (keys %{$DP_output->{$author}->{titles}}) {
-      my @urls = reverse @{$DP_output->{$author}->{titles}->{$title}->{urls}}; # descending order
-      my %title_hash = ( 
-        title => $title,
-        urls => \@urls,
-      );
-      push @titles, \%title_hash;
-    }
-    my %author_hash = ( 
-      author => $author,
-      stories => \@titles,
-      LineNumber => $DP_output->{$author}->{LN},
-    );
-
-    push @$output2, \%author_hash;
-  }
-  $output2 = {masterbin => $output2};
-#print Dumper($output2);
-#----- EXTERNAL DATASTRUCTS -----{{{1
-  #==|| XML
-  my $xml = XMLout($output);
-  my $xml2 = XMLout($output2,NoAttr => 1);
-  #==|| JSON
-  my $json_obj = JSON->new->allow_nonref;
-  my $json = $json_obj->pretty->encode($output);
-  my $json_obj2 = JSON->new->allow_nonref;
-  $json_obj2 = $json_obj2->canonical([1]);
-  my $json2 = $json_obj2->pretty->encode($output2);
-  #print $json2;
-#----- WRITING TO FILES -----{{{1
-  open(my $fh_MasterBinXML, '>' , $fname_MasterBinXML) or die $!;
-    print $fh_MasterBinXML $xml2;
-  close($fh_MasterBinXML);
-  open(my $fh_MasterBinjson, '>' , $fname_MasterBinjson) or die $!;
-    print $fh_MasterBinjson $json2;
-  close($fh_MasterBinjson);
-
-#----- META -----{{{1
-my $cmd = q{sed -n 's/^>\(.*\)/\1/p' ../masterbin.txt > ../story_list.txt};
-system($cmd);
-my $cmd2 = q{sed -n 's/^[Bb]y \(.*\)/\1/p' ../masterbin.txt > ../author_list.txt};
-system($cmd2);
-
-#----- SUBROUTINES -----{{{1
-sub uniq {
-    my %seen;
-    grep !$seen{$_}++, @_;
-}
+##----- BEAUTIFY -----{{{1
+#  my $DP_output = dclone($output); # Deep Copy of $output
+#  my $output2 = [];                # New Better Hash
+#  for my $author (keys %$DP_output) {
+#    my @titles;
+#    for my $title (keys %{$DP_output->{$author}->{titles}}) {
+#      my @urls = reverse @{$DP_output->{$author}->{titles}->{$title}->{urls}}; # descending order
+#      my %title_hash = ( 
+#        title => $title,
+#        urls => \@urls,
+#      );
+#      push @titles, \%title_hash;
+#    }
+#    my %author_hash = ( 
+#      author => $author,
+#      stories => \@titles,
+#      LineNumber => $DP_output->{$author}->{LN},
+#    );
+#
+#    push @$output2, \%author_hash;
+#  }
+#  $output2 = {masterbin => $output2};
+##print Dumper($output2);
+##----- EXTERNAL DATASTRUCTS -----{{{1
+#  #==|| XML
+#  my $xml = XMLout($output);
+#  my $xml2 = XMLout($output2,NoAttr => 1);
+#  #==|| JSON
+#  my $json_obj = JSON->new->allow_nonref;
+#  my $json = $json_obj->pretty->encode($output);
+#  my $json_obj2 = JSON->new->allow_nonref;
+#  $json_obj2 = $json_obj2->canonical([1]);
+#  my $json2 = $json_obj2->pretty->encode($output2);
+#  #print $json2;
+##----- WRITING TO FILES -----{{{1
+#  open(my $fh_MasterBinXML, '>' , $fname) or die $!;
+#    print $fh_MasterBinXML $xml2;
+#  close($fh_MasterBinXML);
+#  open(my $fh_MasterBinjson, '>' , $fname) or die $!;
+#    print $fh_MasterBinjson $json2;
+#  close($fh_MasterBinjson);
+#
+##----- META -----{{{1
+#my $cmd = q{sed -n 's/^>\(.*\)/\1/p' ../masterbin.txt > ../story_list.txt};
+#system($cmd);
+#my $cmd2 = q{sed -n 's/^[Bb]y \(.*\)/\1/p' ../masterbin.txt > ../author_list.txt};
+#system($cmd2);
+#
+##----- SUBROUTINES -----{{{1
+#sub uniq {
+#    my %seen;
+#    grep !$seen{$_}++, @_;
+#}
+#sub argParser {
+#}
