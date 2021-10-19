7490a73dc52e41eb12aedc8e3fcedeafafb91e9d
Azuhmier <Azuhmier@Azuhmiers-MacBook-Pro.local>
2021-10-19 11:57:51 -0500

    save

--- code/c_test/hmofa3.c
+++ code/c_test/hmofa3.c
@@ -1,37 +1,37 @@
 //#!gcc %.c -o %.out_
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <float.h>
 
-int main(int argc, char** argv)
+int main()
 {
+     {
+         FILE *fp;
+         char buff[255];
 
-   // Enumeration
-   enum Days_of_week {Sun, Mon, Tue, Wed = 4, Thur, Fri, Sat};
-   enum Days_of_week day = Wed;
-   printf("day is %d.\n", day);
-   int i; for (i=Sun; i<=Sat; i++) printf("%d ", i);
-   printf("\n\n");
+         fp = fopen("../test/masterbin.txt", "r");
+         fscanf(fp, "%s", buff);
+         printf("1 : %s\n", buff );
 
-   // Strings and Chars
-   char name[]  = "sam";
-   char name2[] = {'a','n','o','n','\0'};
-   char letter  = 'a';
-   printf("my name is %s also known as %s\n", name, name);
-   printf("my name is %s also known as %s\n", name2, name2);
-   printf("my name is %c also known as %c\n", letter, letter);
-   printf("itoddler BTFO\n\n");
+         fgets(buff, 255, (FILE*)fp);
+         printf("2: %s\n", buff );
+         fgets(buff, 255, (FILE*)fp);
+         printf("3: %s\n", buff );
+         fclose(fp);
+     }
+     {
+         FILE *f = fopen("../test/masterbin.txt", "rb");
+         fseek(f, 0, SEEK_END);
+         long fsize = ftell(f);
+         printf("\nfsize: %ld\n", fsize);
 
-   //type casting
-   float a = 1.2;
-   //int b  = a; //Compiler will throw an error for this
-   int b = (int)a + 1;
-   float c = a + (float)1;
-   printf("Value of a is %f\n",a);
-   printf("Value of b is %d\n",b);
-   printf("Value of b is %f\n",c);
-
+         fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+         char *string = malloc(fsize);
+         fread(string, 1, fsize, f);
+         fclose(f);
+         int max = (int)sizeof string;
+         printf("MAX: %d\n", max);
+         int i; for (i=0; i<=fsize; i++) printf("%c", string[i]);
+     }
 
 
    return 0;
