7495714e8fdafd7fef294a92fa15df5183ce524d
Azuhmier <Azuhmier@Azuhmiers-MacBook-Pro.local>
2022-04-24 20:08:37 -0500

    update

--- code/test/lib/Ohm/Controller.pm
+++ code/test/lib/Ohm/Controller.pm
@@ -1,134 +1,486 @@
 package Ohm::Controller;
+use parent 'Ohm::Main';
+
 use warnings;
 use strict;
-use File::Glob;
-use Cwd;
+use utf8;
+use Carp qw(croak carp confess);
+use Storable qw(dclone);
+use Cwd qw (getcwd abs_path);
+use File::Spec;
 use File::Basename;
-use Carp qw(croak carp);
-my $erreno;
-#./.ohm dir; basically like git;
-  # history; full, like git
-# repo
-  # dspts
-  # jsons
-  # drsr
-  # stdout
+use File::Find;
+use JSON::XS;
+use Array::Utils qw(:all);
+use Data::Dumper;
 
-sub new #{{{1
+use lib ($ENV{HOME}.'/hmofa/hmofa/code/test/lib');
+#use Ohm::HasherWIP;
+
+use constant FALSE => 1==0;
+use constant TRUE  => not FALSE;
+use constant BASE  => '.ohmi';
+
+
+#############################################################
+#  PUBLIC
+#############################################################
+
+sub init_ohmi #{{{1
 {
-    my $class = shift @_;
+    my ($self, $baseDir) = @_;
 
-    my $self = {};
-    bless $self, $class;
+    croak "ERROR: '.ohmi' directory has not been selected\n"
+        unless ( $self->get('status','base') );
 
-    $self->__init();
+    $baseDir = $baseDir // $self->{cwd};
+    my $basePath = $baseDir . '/' . BASE;
+
+    mkdir $basePath;
+    my $fname =  $basePath.'/ohminfo';
+
+    $self->__write($fname,["lol\n", "go away\n"]);
+
+    return $self;
+
+}
+
+
+
+sub select_ohmi #{{{1
+{
+    my ($self, $path, $NONE) = @_;
+
+    die "ERROR: ohmi path not specified\n"
+            unless $path;
+
+    unless ($NONE)
+    {
+        $self->{paths}{baseDir} = $path;
+        $self->__set_status('base',TRUE);
+    }
+
+    else
+    {
+        $self->{paths}{baseDir} = undef;
+        $self->__set_status('base',FALSE);
+    }
 
     return $self;
 }
 
-sub get_status #{{{1
+sub seek_ohmi #{{{1
+{
+    my ( $self, $path, $mode ) = @_;
+
+    my $baseDirs = $self->__find_ohmi();
+    my @basePaths = map { $_.'/'.BASE } @$baseDirs;
+
+    return  \@basePaths;
+
+    # Find .ohm in current dir or upwards
+    sub __find_ohmi #{{{
+    {
+        my $self = shift @_;
+        my $dir = $self->{cwd};
+        my $baseDirs = [];
+
+        if (-d BASE)
+        {
+            push @$baseDirs, $dir;
+        }
+        else
+        {
+
+            while ( $dir )
+            {
+                my $hit_top = $dir eq dirname($dir);
+                $dir = dirname($dir);
+
+                my $found  = basename($dir) eq BASE;
+
+                if ($found or $hit_top)
+                {
+                    push @$baseDirs, $dir if $found;
+                }
+            }
+        }
+
+        return $baseDirs;
+
+    } #}}}
+
+}
+
+
+
+
+sub importFromPaths #{{{1
+{
+    my ($self) = @_;
+
+    my $paths = dclone $self->{paths};
+
+
+    delete $paths->@{"his","baseDir"};
+
+    my $data = {};
+    for my $key (keys %$paths)
+    {
+        next unless $paths->{$key};
+
+        my $isArray = UNIVERSAL::isa($paths->{$key}, 'ARRAY' );
+        if ($isArray)
+        {
+            my $paths = $paths->{$key};
+            next unless @$paths;
+
+            for my $path ( @$paths )
+            {
+                my ($ext) = $path =~ /(\.[^.]*$)/g;
+                $ext = $ext // '';
+
+                if ($ext eq '.json')
+                {
+                    push $data->{$key}->@*, $self->__importJson($path);
+                }
+                elsif ($ext eq '.txt')
+                {
+                    __read($path);
+                    push $data->{$key}->@*, $self->__read($path);
+                }
+                else
+                {
+                    croak "ERROR: Invalid file type '$ext'";
+                }
+            }
+        }
+        else
+        {
+            my $path = $paths->{$key};
+
+            my ($ext) = $path =~ /(\.[^.]*$)/g;
+            $ext = $ext // '';
+
+            if ($ext eq '.json')
+            {
+                $data->{$key} = $self->__importJson($path);
+            }
+            elsif ($ext eq '.txt')
+            {
+                $data->{$key} = $self->__read($path);
+            }
+            else
+            {
+                croak "ERROR: Invalid file type '$ext'";
+            }
+        }
+
+    }
+
+    return $data;
+}
+
+
+
+
+sub launch #{{{1
+{
+    my ($self) = @_;
+    return $self;
+}
+
+
+
+
+sub genDb #{{{1
+{
+    my ($self, $args) = @_;
+    my $db = Ohm::HasherWIP->new($args);
+    return $db;
+}
+sub commit #{{{1
+{
+    my ($self) = @_;
+    return $self;
+}#}}}
+
+
+
+
+#############################################################
+# PRIVATE
+#############################################################
+
+sub __init #{{{1
 {
     my ( $self, $args ) = @_;
+    $self->__private(caller);
 
-}
-sub __init { #{{{1
-    my ( $self, $args ) = @_;
-    my $class = ref $self;
-    $self->{state} = '';
-    unless (UNIVERSAL::isa($args->[0], 'HASH')) {
-        $args->[0] = {
-            name => $args->[0],
-            base => $args->[1],
-        };
-    }
-    my %args  = $args->[0]->%*;
+    $self->__set_args( $args , 1);
 
+    $self->__set_status
+    ({
+        state => 'ok',
+        init => TRUE,
+    });
 
-    #%--------ARGS--------#%
-    # CHECK NAME
-    my $name = delete $args{name};
-    unless ($name) {
-        $name = 'ohm_lib';
-    } elsif (ref $name) {
-        croak("$class requires a name that is a scalar string");
-    }
-    $self->{name} = $name;
-
-    # CHECK BASE
-    my $base = delete $args{base};
-    unless ($base) {
-        $base = Cwd::abs_path('.');
-    } elsif (ref $base) {
-        croak("$class requires a base that is a scalar string");
-    } else {
-        $base = Cwd::abs_path( glob $base =~ s/\/$//r );
-    }
-    $self->{paths}{base} = $base;
-
-
-    #%--------NON ARGS--------#%
-    # CHECK KEYS
-    if ( my $remaining = join ', ', keys %args ) {
-        croak("Unknown keys to $class\::new: $remaining");
-    }
-
-    # LIB_DIR
-    my $lib_dir = $base.'/'.$name;
-    $lib_dir = __genUniqueDir($lib_dir);
-    $self->{paths}{lib_dir} = $lib_dir;
-
-    # DEBUG
-    $self->{debug} = [];
-    $SIG{__DIE__} = sub {
-        print $_ for $self->{debug}->@*;
-        print $erreno if $erreno;
-    };
+    return $self;
 }
 
-sub set_args { #{{{1
-}
+sub __set_args #{{{1
+{
 
-sub __checkChgArgs { #{{{1
-    my ($arg, $cond, $type) = @_;
-    unless ( defined $arg ) {
-        croak( (caller(1))[3] . " requires an input" );
-    } elsif (ref $arg ne $cond) {
-        croak( (caller(1))[3] . " requires a $type" );
+    my ( $self, $args, $clear ) = @_;
+    $self->__private(caller);
+
+    my $old_args = $self->{args};
+
+    if ($old_args && !$clear)
+    {
+        $self->__checkArgs( {args => $args} );
+        $args = __merge($args, $old_args);
     }
-}
 
-sub __genUniqueDir { #{{{1
-    my $dir = shift @_;
-    my $cnt = 0;
-    my $new_dir = $dir;
-    while (-d $new_dir) {
-        $cnt++;
-        $new_dir = $dir.'-'.$cnt;
+    else
+    {
+        $self->__checkArgs( {args => $args}, $clear);
+        print Dumper $args;
     }
-    return $new_dir;
+
+    $self->{args} = $args;
+
+
+
+    ## PATHS
+    my $paths = $self->{$args}{paths};
+
+    for my $key (keys %$paths)
+    {
+        my $ref = ref $paths->{$key};
+
+        if ( $ref eq 'ARRAY' )
+        {
+            $self->{paths}{$key}->@* = map { abs_path $_ } @{ $paths->{key} }
+        }
+
+        else
+        {
+            $self->{paths}{$key} = abs_path $paths->{$key}
+        }
+    }
+
+    ## OPTS
+    $self->{opts} = $self->{args}{opts};
+
+    ## CONFIGS
+    $self->{configs} = $self->{args}{configs};
+
+    ## FLAGS
+    $self->{flags} = $self->{args}{flags};
+
+    return $self;
 }
 
-1;
 
-### SETUP
-#my $CWD = Cwd::cwd();
-#my $CONFIG_DIR = glob '~/.ohm/.config';
-#my $BASE_DIR = 0;
-#{
-#    open my $fh, '<', $CONFIG_DIR
-#        or die 'something happened';
-#    while (my $line = <$fh>)  {
-#        if ($line =~ m/^BASE=(.*)/) {
-#            $BASE_DIR = glob $1;
-#            last;
-#        }
-#    }
-#}
-#my $LIB_DIR = $BASE_DIR.'/lib';
-#
-#print $BASE_DIR,"\n";
-#print $LIB_DIR,"\n";
-#print $CWD,"\n";
-#print "\n";
-#
-#1;
+
+
+sub __bps #{{{1
+{
+    my $self = shift @_;
+    $self->__private(caller);
+
+    my %bps =
+    (
+        self => #{{{2
+        {
+            fill => 0,
+            member => {},
+            general =>
+            {
+                args    => {},
+                db      => {},
+                paths   => {},
+                opts    => {},
+                configs => {},
+                status  => {
+                               state => '',
+                               init => FALSE,
+                               args => FALSE,
+                               base => FALSE,
+                               sync => FALSE,
+                            },
+                flags   => {},
+            },
+        },
+
+        args => #{{{2
+        {
+            fill => 0,
+            member => {},
+            general =>
+            {
+                paths   => {},
+                opts    => {},
+                configs => {},
+                flags   => {},
+            },
+        },
+
+        paths => #{{{2
+        {
+            fill => 0,
+            member => {},
+            general =>
+            {
+                drsr      => $self->{paths}{baseDir}
+                                 ? $self->{paths}{baseDir}.'/'.BASE.'/db/drsr.json'
+                                 : '',
+                dspt      => $self->{paths}{baseDir}
+                                 ? $self->{paths}{baseDir}.'/'.BASE.'/db/dspt.json'
+                                 : '',
+                input     => $self->{paths}{baseDir}
+                                 ? $self->{paths}{baseDir}.'/'.BASE.'/db/input.txt'
+                                 : '',
+                mask      => $self->{paths}{baseDir}
+                                 ? $self->{paths}{baseDir}.'/'.BASE.'/db/mask.json'
+                                 : '',
+                smask     => [],
+
+                sdrsr     => [],
+
+                extern    => [],
+
+                his       => $self->{paths}{baseDir}
+                                 ? $self->{paths}{baseDir}.'/'.BASE.'/db/his'
+                                 : '',
+                baseDir   => undef,
+            },
+        },
+
+        opts => #{{{2
+        {
+            fill => 0,
+            member => {},
+            general =>
+            {
+                plhd => {},
+                prsv => {},
+            },
+        },
+
+        configs => #{{{2
+        {
+            fill => 0,
+            member => {},
+            general =>
+            {
+                writable => FALSE,
+            },
+        },
+
+        flags => #{{{2
+        {
+            fill => 0,
+            member => {},
+            general =>
+            {
+                launch => FALSE,
+                commit => FALSE,
+            },
+        },
+
+
+        ohminfo => #{{{2
+        {
+            fill => 0,
+            member => {},
+            general =>
+            {
+                updated => '',
+                created => '',
+                name    => '',
+            },
+        },#}}}
+
+    );
+
+    return \%bps;
+}#}}}
+
+
+
+
+#############################################################
+#  UTILITIES
+#############################################################
+
+sub __write #{{{1
+{
+    my ($self, $fname, $lines) = @_;
+    $self->__private(caller);
+    open my $fh, '>:utf8', $fname
+        or die "cannot open $fname";
+        print $fh @$lines;
+        truncate $fh, tell($fh) or die;
+        seek $fh,0,0 or die;
+    return $self;
+}
+
+
+
+
+sub __writeJson #{{{1
+{
+    my ($self,$path,$hash) = @_;
+    $self->__private(caller);
+    my $encoder = JSON::XS->new;
+    $encoder->pretty->allow_nonref->allow_blessed(['true']);
+    my $json = $encoder->encode( $hash );
+    $self->__write($json,$path);
+}
+
+
+
+
+sub __fileExist #{{{
+{
+    my ($self, $path) = @_;
+    $self->__private(caller);
+
+    return 0 unless $path;
+
+    my @cwd   = File::Spec->splitdir( $self->{cwd} );
+    my @path  = File::Spec->splitdir( $path );
+
+    no warnings 'uninitialized';
+    my @isect  = intersect(@path, @cwd);
+    use warnings 'uninitialized';
+
+    my $dir = File::Spec->catdir(@isect);
+    my $idx = scalar @isect;
+
+
+    for ( $idx .. $#path )
+    {
+        chdir $dir;
+        unless (-e $path[$_])
+        {
+            chdir $self->{cwd} or die;
+            return 0;
+        }
+        no warnings 'uninitialized';
+        $dir = File::Spec->catdir(@path[0 .. $_]);
+        use warnings 'uninitialized';
+    }
+    chdir $self->{cwd};
+    return 1;
+}
+
+
+
+
+
+
+
+1 ;
